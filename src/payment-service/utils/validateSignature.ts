3224e9b06128b7cff8f313c7ddbd7bd34fe34396
90felipe09 <Felhipeshiraishi@gmail.com>
2021-09-05 09:34:42 -0300

    fix: request validation

--- src/payment-service/utils/validateSignature.ts
+++ src/payment-service/utils/validateSignature.ts
@@ -1,8 +1,9 @@
-import crypto from 'crypto'
+import crypto from 'crypto';
+import { SHA256 } from 'crypto-js';
 import { Response } from 'express';
 import { DeclarationCreationArgs } from '../data/DeclarationDTO';
 import { ErrorMessage } from '../data/ErrorMessage';
-import { RedeemArguments } from '../data/RedeemDTO';
+import { CommitmentMessage, RedeemArguments } from '../data/RedeemDTO';
 import { SignedRequest } from '../data/SignedDTO';
 import { User } from '../models/User';
 
@@ -11,9 +12,9 @@ const SIGNATURE_ALGORITHM = "ECDSA";
 const HASHING_ALG_NAME = "SHA-256";
 const SIGNATURE_FORMAT = "hex";
 
-export const validateSignature = (json: string, certificate: string, signature: string): boolean => {
+export const validateSignature = (jsonHash: string, certificate: string, signature: string): boolean => {
     const verify = crypto.createVerify(ALGORITHM);
-    const buffer = Buffer.from(json);
+    const buffer = Buffer.from(jsonHash);
     
     verify.update(buffer)
     
@@ -25,7 +26,9 @@ export const validateSignature = (json: string, certificate: string, signature: 
 export const validateInRequest = async (signedRequest: SignedRequest<RedeemArguments | DeclarationCreationArgs>, res: Response) => {
     const user = await User.findOne(signedRequest.fingerprint);
     if (user){
-        if (!validateSignature(JSON.stringify(signedRequest.content), user?.publicKey, signedRequest.signature)){
+        const requestString = JSON.stringify(signedRequest.content);
+        const requestHash = SHA256(requestString).toString();
+        if (!validateSignature(requestHash, user?.publicKey, signedRequest.signature)){
             const errorMessage: ErrorMessage = {
                 message: "Invalid request"
             }
@@ -43,4 +46,10 @@ export const validateInRequest = async (signedRequest: SignedRequest<RedeemArgum
         res.status(400).send(errorMessage)
         return false
     }
+}
+
+export const validateCommitment = (commitment: CommitmentMessage, res: Response, certificate: string) => {
+    const commitmentContentString = JSON.stringify(commitment.data);
+    const commitmentHash = SHA256(commitmentContentString).toString();
+    return validateSignature(commitmentHash, certificate, commitment.signature);
 }
